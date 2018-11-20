9eef110f0309e78b798e5fd441bd7bce27fe5d30
khoursaaf <khoursa@gmail.com>
2018-11-20 16:18:59 +0100

    correction

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Exercise</title>
-    </head>
-    <body>
-        <p>Créer une variable km. L'initialiser à 1. Afficher son contenu.</p>
-        <p><?php $km = 1; echo $km; ?></p>
-        <p>Changer sa valeur par 3. Afficher son contenu.</p>
-        <?php $km = 3; echo $km;  ?>
-        <p>Changer sa valeur par 125. Afficher son contenu.</p>
-        <?php $km = 125; echo $km;  ?>
-    </body>
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercise</title>
+  </head>
+  <body>
+    <p>Créer une variable km. L'initialiser à 1. Afficher son contenu.</p>
+    <?php $km = 1; echo $km; ?>
+    <p>Changer sa valeur par 3. Afficher son contenu.</p>
+    <?php $km = 3; echo $km;  ?>
+    <p>Changer sa valeur par 125. Afficher son contenu.</p>
+    <?php $km = 125; echo $km;  ?>
+  </body>
 </html>
